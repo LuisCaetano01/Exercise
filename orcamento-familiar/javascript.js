5460abcdaaa0a062d75a2a71f493d7ef5cbfe434
LuisCaetano01 <117701459+LuisCaetano01@users.noreply.github.com>
2022-12-20 04:09:03 +0000

    evoluçao

--- orcamento-familiar/javascript.js
+++ orcamento-familiar/javascript.js
@@ -5,15 +5,24 @@ const btn_concluir=document.querySelector("#btnConcluir")
 const tbodyCampos=document.querySelector("tbody")
 
 
-//Var da função addField
-var n_campos=5
+function iniciarPagina(){
+    n_campos = 5
+    addFields()
+}
 
+function btn_func(){
+    n_campos = 1
+    console.log(n_campos)
+    addFields()
+}
+
+
+var n_campos=0  //Var da função addField
+//Função para criar todos os campos de input
 function addFields(){
     const container = document.getElementById("orcamentoCampos")    
 
     for (var i = 0;i<n_campos;i++){
-        //container.appendChild(document.createTextNode("campo" + (i+1)))
-
         //Cria campo descrição
         let td1 = document.createElement("td")
         let input = document.createElement("input")
@@ -22,22 +31,30 @@ function addFields(){
         input.minLength = 50
         input.size = 50
         td1.appendChild(input)
-
         //Cria campo valor
         let td2 = document.createElement("td")
         let input2 = document.createElement("input")
         input2.type = "number"
         input2.id = "valor" + (i+1)
         td2.appendChild(input2)
-
-        //Cria campo tipo
+        //Cria campo MENU SELECT para o tipo Activo/PAssivo
         let td3 = document.createElement("td")
-        const boxOpt =  selectBox.options [selectBox.selectedIndex];
-        boxOpt[0].text = "a Pagar"
-        boxOpt[0].value = "Passivo"
+        let boxOpt = document.createElement("SELECT") //cria Select 
+        boxOpt.setAttribute("id", "mySelect");
+        //cria option 1
+        var option1 = document.createElement("option"); 
+        option1.setAttribute("value", "Activo");
+        var option1text = document.createTextNode("a Receber");
+        option1.appendChild(option1text);
+        //cria option 2
+        var option2 = document.createElement("option");
+        option2.setAttribute("value", "Passivo");
+        var option2text = document.createTextNode("a Pagar");
+        option2.appendChild(option2text);
 
-        td3.appendChild(boxOpt)
-
+        boxOpt.appendChild(option1); //Insere os campos OPTION no SELECT
+        boxOpt.appendChild(option2);
+        td3.appendChild(boxOpt) //Insere o campo no TD (tabela)
 
         //adiciona os campos na Div
         container.appendChild(td1)        
@@ -45,18 +62,13 @@ function addFields(){
         container.appendChild(td3)  
 
         //Faz parágrafo
-        const para = document.createElement("br")
-        container.appendChild(para)   
+        const paragrafo = document.createElement("br")
+        container.appendChild(paragrafo)   
 
-        console.log("ola" + i)
 
     }
 }
-/*                        <td><input type="number" name="valor" step="0.01" min="0.00"></td>
-                        <td><select name="tipo" size="1">
-                                <option value="A">a Receber</option>
-                                <option value="P">a Pagar</option>
-                            </select></td>
-*/
-btn_campos.addEventListener("click", addFields())
-n_campos=1
+
+
+btn_campos.addEventListener("click", btn_func)
+btn_concluir.addEventListener("load", iniciarPagina())
