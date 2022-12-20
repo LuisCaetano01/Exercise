82578fd483eb8ce3b5f674ba59c921de573bb77b
LuisCaetano01 <117701459+LuisCaetano01@users.noreply.github.com>
2022-12-20 02:12:44 +0000

    update

--- orcamento-familiar/javascript.js
+++ orcamento-familiar/javascript.js
@@ -1,28 +1,62 @@
-const todosCampos=[...document.querySelectorAll(".orcamentoCampos")] //Campos do Orçamento | n está a ser usado.
+const todosCampos=document.querySelectorAll(".orcamentoCampos") //Campos do Orçamento | n está a ser usado.
 const inserirCampos=document.querySelector(".orcamentocriado")
 const btn_campos=document.querySelector("#btnAumentarCampos")
 const btn_concluir=document.querySelector("#btnConcluir")
 const tbodyCampos=document.querySelector("tbody")
 
 
-// Captura os 3 elementos input da div, e assim poderei obte-los pelo índice, por ser o mesmo número.
-const camposDescricao = [...document.getElementsByName("descricao")] 
-const camposValor = [...document.getElementsByName("valor")]     
-const camposTipo = document.getElementsByName("tipo")      
+//Var da função addField
+var n_campos=5
 
-camposDescricao.map((elm,ind,cd)=>{
-    document.write(elm.value + " ------------ " + camposValor[ind].value + "<br>")
-    document.write(camposValor[ind].value)
+function addFields(){
+    const container = document.getElementById("orcamentoCampos")    
 
-    
-})
-const fm=document.forms["f_orcamento"]
-document.write(fm.innerHTML)
-document.write(fm.innerHTML)
-document.write(fm.innerHTML)
+    for (var i = 0;i<n_campos;i++){
+        //container.appendChild(document.createTextNode("campo" + (i+1)))
 
-console.log(fm.innerHTML)
+        //Cria campo descrição
+        let td1 = document.createElement("td")
+        let input = document.createElement("input")
+        input.type = "text"
+        input.id = "descricao" + (i+1)
+        input.minLength = 50
+        input.size = 50
+        td1.appendChild(input)
 
-console.log(todosCampos)
-console.log(camposDescricao)
-console.log(tbodyCampos)
+        //Cria campo valor
+        let td2 = document.createElement("td")
+        let input2 = document.createElement("input")
+        input2.type = "number"
+        input2.id = "valor" + (i+1)
+        td2.appendChild(input2)
+
+        //Cria campo tipo
+        let td3 = document.createElement("td")
+        const boxOpt =  selectBox.options [selectBox.selectedIndex];
+        boxOpt[0].text = "a Pagar"
+        boxOpt[0].value = "Passivo"
+
+        td3.appendChild(boxOpt)
+
+
+        //adiciona os campos na Div
+        container.appendChild(td1)        
+        container.appendChild(td2)
+        container.appendChild(td3)  
+
+        //Faz parágrafo
+        const para = document.createElement("br")
+        container.appendChild(para)   
+
+        console.log("ola" + i)
+
+    }
+}
+/*                        <td><input type="number" name="valor" step="0.01" min="0.00"></td>
+                        <td><select name="tipo" size="1">
+                                <option value="A">a Receber</option>
+                                <option value="P">a Pagar</option>
+                            </select></td>
+*/
+btn_campos.addEventListener("click", addFields())
+n_campos=1
